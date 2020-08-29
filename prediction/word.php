824f0d9150511827f9b105967bd5f23aa12aad35
Margot <margot07008>
2020-08-29 23:03:51 +0300

    creating img

---
+++ prediction/word.php
@@ -0,0 +1,26 @@
+﻿<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta http-equiv="Content-Language" Content="ru">
+<head>
+	<link href="favicon.ico" rel="shortcut icon" type="image/x-icon" />
+	<title>Слово дня</title>
+	<link href="action/css/style.css" rel="stylesheet">
+</head>
+<center>
+		<H1><b>ШАГ 2</b></H1>
+		<H2><b>Заполни поля:</b></H2>
+	<form action="genimage.php" method="post">
+		<p>Слово: <input type="text" name="word" /></p>
+		<p>Его значение: </br><textarea name="fact" cols="60" rows="10" maxlength="10000"></textarea></p>
+		<p><input type="submit" class="button" value="Создать"/></p>	
+		<p><a href="/action/adminword.php" class="button">Открыть хранилище постов</a><p>
+		<p><a href="/helper.php" class="button" target="_blank">Подручный :д</a></p>
+        <H5><li>Чтобы выделить часть текста курсивом введи "/".</li></br>
+В примере ниже курсивом выделится "сон".</br>" Это был /сон/." 
+</br>(актуально только для русских букв)</br>
+        </br><li>Чтобы перейти на новую строку нажми Enter </li></br>
+        </br><li>Чтобы поставить ударение добавь запятую после ударной.</li>
+</br> "корОва" - "коро,ва" </br></br></H5>
+<H4><a href="/">Назад</a></H4>
+</center>
+</body>
+</html>
